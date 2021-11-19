b3f04dbd25acdef8390636ecbb0f44e2099051e4
John J Quiroga O <jjqojj@outlook.com>
2021-11-19 17:35:25 -0500

    Authentication microservice developed according to the MisionTic guidelines. Ready to create a docker image and deploy to heroku.

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!C:\Users\jjqoj\Documents\JJQOJJ\0. AGE\5. M_TIC_2022\C4\P\LAB\bank_be_C4\env\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
